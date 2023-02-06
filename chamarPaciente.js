d795cc707265955e9ebcb4ddeeea661269a745d3
kainanps <kainan.paiva777@gmail.com>
2023-02-06 20:48:14 -0300

    new commit

--- chamarPaciente.js
+++ chamarPaciente.js
@@ -1,11 +1,11 @@
-// setInterval(function(){
-//     //location.reload();
-//  }, 10000);
-busca()
+setInterval(function(){
+    busca()
+ }, 1000);
 if(localStorage.getItem('paciente1')){
     localStorage.setItem('paciente1', '')
     localStorage.setItem('paciente2', '')
     localStorage.setItem('paciente3', '')
+    localStorage.setItem('paciente4', '')
 }
 
 function busca(){
@@ -20,13 +20,36 @@ function busca(){
 }
 
 function chamarPaciente(jsonData){
-    let paciente = JSON.parse(jsonData)
+    const paciente = JSON.parse(jsonData)
     if(localStorage.getItem('paciente1') != paciente.Paciente){
+        
+        localStorage.setItem('paciente4', localStorage.getItem('paciente3'))
 
-        localStorage.setItem('paciente1', paciente.Paciente)
+        localStorage.setItem('paciente3', localStorage.getItem('paciente2'))
+
         localStorage.setItem('paciente2', localStorage.getItem('paciente1'))
 
+        localStorage.setItem('paciente1', paciente.Paciente)
+        falar(localStorage.getItem('paciente')+" Comparecer ao consultório 1")
         
+
+        const paciente1 = document.querySelector('.paciente1 p span')
+        paciente1.innerText = "Nome: " + localStorage.getItem('paciente1')
+
+        const paciente2 = document.querySelector('.paciente2 p span')
+        paciente2.innerText = "Nome: " + localStorage.getItem('paciente2')
+
+        const paciente3 = document.querySelector('.paciente3 p span')
+        paciente3.innerText = "Nome: " + localStorage.getItem('paciente3')
+
+        const paciente4 = document.querySelector('.paciente4 p span')
+        paciente4.innerText = "Nome: " + localStorage.getItem('paciente4')
     }
 }
 
+function falar(texto){
+    const synth = window.speechSynthesis
+    var toSpeak = new SpeechSynthesisUtterance(texto)
+    toSpeak.voice = synth.getVoices()[0]
+    synth.speak(toSpeak)
+}
